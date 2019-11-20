60468d2fc9bc1c0932d94ffa3a4bb31e97cf26a4
Ankit Patil <ankit.patil@neosofttech.com>
2019-11-20 19:56:27 +0530

    Added docstring to the function.

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
+"""
+This module will allow client to connect with the server ip and Broadcast Message
+"""
 import zmq
 
 
 def bind_server():
+    """
+    This function binds socket to the ip.
+    :return:
+    """
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://0.0.0.0:5555")
