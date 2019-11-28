8143e0fae940c781cde244b00961535610cbfbe2
Ankit Patil <ankit.patil@neosofttech.com>
2019-11-28 18:20:12 +0530

    Implemented JWT authentication.

--- server.py
+++ server.py
@@ -3,20 +3,71 @@ This module will allow client to connect with the server ip and Broadcast Messag
 """
 import zmq
 from flask import Flask, render_template, request
-
-app = Flask(__name__)
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+from models import app, db, User
 
 context = zmq.Context()
 zmq_socket = context.socket(zmq.PUB)
 
 
+def authenticate(username, password):
+    req_dict = request.get_json()
+    user = User.query.filter_by(username=req_dict.get('username')).first()
+    password = req_dict.get('password')
+    if user and safe_str_cmp(user.hash_password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return 1
+
+
+jwt = JWT(app, authenticate, identity)
+
+
 @app.route('/')
+@jwt_required()
 def index():
     """
     This functions redirect to home.
     :return:
     """
     return render_template('index.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def user_login():
+    if request.method == "POST":
+        req_dict = request.get_json()
+        # req_dict = request.form
+        user = User.query.filter_by(username=req_dict.get('username')).first()
+        if user:
+            return render_template('home.html', context=user.username)
+        return "Check username/password!"
+
+    else:
+        return "Method not allowed"
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def create_user():
+    print(request)
+    if request.method == "POST":
+        request_dict = request.get_json()
+        # request_dict = request.form
+        print(request_dict.get('username'))
+        user = User(username=request_dict.get('username'), email=request_dict.get('email'),
+                    hash_password=request_dict.get('password'))
+        db.session.add(user)
+        db.session.commit()
+        db.session.remove()
+        # return render_template('login.html')
+        return "User created successfully!"
+    else:
+        # return render_template('register.html')
+        pass
 
 
 @app.route('/create', methods=['POST'])
@@ -48,4 +99,5 @@ def send_message():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0')
