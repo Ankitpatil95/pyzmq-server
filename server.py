0b37afca264f9d0f6782c1cc65833285d6b8c7c0
Ankit Patil <ankit.patil@neosofttech.com>
2019-11-28 19:28:56 +0530

    Return json for register function.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 This module will allow client to connect with the server ip and Broadcast Message
 """
 import zmq
-from flask import Flask, render_template, request
+from flask import request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 from models import app, db, User
@@ -21,7 +21,8 @@ def authenticate(username, password):
 
 def identity(payload):
     user_id = payload['identity']
-    return 1
+    print(user_id)
+    return User.query.filter_by(id=user_id).first()
 
 
 jwt = JWT(app, authenticate, identity)
@@ -34,39 +35,41 @@ def index():
     This functions redirect to home.
     :return:
     """
-    return render_template('index.html')
+    return "Home"
 
 
-@app.route("/login", methods=['POST', 'GET'])
-def user_login():
-    if request.method == "POST":
-        req_dict = request.get_json()
-        # req_dict = request.form
-        user = User.query.filter_by(username=req_dict.get('username')).first()
-        if user:
-            return render_template('home.html', context=user.username)
-        return "Check username/password!"
-
-    else:
-        return "Method not allowed"
+# @app.route("/login", methods=['POST', 'GET'])
+# def user_login():
+#     if request.method == "POST":
+#         req_dict = request.get_json()
+#         # req_dict = request.form
+#         user = User.query.filter_by(username=req_dict.get('username')).first()
+#         if user:
+#             return "render_template('home.html', context=user.username)"
+#         return "Check username/password!"
+#
+#     else:
+#         return "Method not allowed"
 
 
 @app.route("/register", methods=['POST', 'GET'])
-def create_user():
+def register():
     print(request)
     if request.method == "POST":
         request_dict = request.get_json()
         # request_dict = request.form
+        print(request_dict)
         print(request_dict.get('username'))
         user = User(username=request_dict.get('username'), email=request_dict.get('email'),
                     hash_password=request_dict.get('password'))
         db.session.add(user)
         db.session.commit()
-        db.session.remove()
         # return render_template('login.html')
-        return "User created successfully!"
+        user_dict = {"username": user.username, "message": "User created successfully"}
+        db.session.remove()
+        return user_dict
     else:
-        # return render_template('register.html')
+        # return jsonify()
         pass
 
 
@@ -78,10 +81,10 @@ def bind_server():
     """
     try:
         zmq_socket.bind("tcp://0.0.0.0:5555")
-        return render_template('send_message.html')
+        return "render_template('send_message.html')"
 
     except Exception as e:
-        return render_template('send_message.html')
+        return "render_template('send_message.html')"
 
 
 @app.route('/send', methods=['GET', 'POST'])
@@ -95,9 +98,9 @@ def send_message():
         zmq_socket.send(msg.encode('utf-8'))
         return "Message sent successfully."
     else:
-        return render_template('send_message.html')
+        return "render_template('send_message.html')"
 
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
